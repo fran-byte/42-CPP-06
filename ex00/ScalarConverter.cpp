c275837699677d26ca066678e8f41651a76627f7
frrommero <frromero@student.42madrid.com>
2025-12-12 14:57:35 +0100

    up

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -6,9 +6,15 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 14:52:18 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/12 14:55:21 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:57:16 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter(){}
+
+ScalarConverter::ScalarConverter(std::string const strNumber)
+{}
+
+ScalarConverter::~ScalarConverter(){}

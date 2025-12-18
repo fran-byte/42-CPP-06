f4a5c289e440f6d9ad74cf2b0eb38c00b5b77dff
fran-byte <fran-byte@hotmail.com>
2025-12-18 13:31:57 +0100

    up

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -162,7 +162,7 @@ void ScalarConverter::convert(const std::string &s) {
     errno=0;
     double val=std::strtod(num.c_str(),&end);
 
-    if (errno==ERANGE) 
+    if (errno==ERANGE) /* RangeError ? */
     {
         std::cout<<"char: impossible\nint: impossible"<<std::endl;
         if (val==HUGE_VAL)
@@ -172,9 +172,12 @@ void ScalarConverter::convert(const std::string &s) {
         else
             std::cout<<"float: 0.0f\ndouble: 0.0"<<std::endl;
     } 
-    else if (end==num.c_str())    
-        std::cout<<"Invalid input"<<std::endl;    
-    else 
-        displayResults(val);    
+    else if (end==num.c_str()) 
+   
+        std::cout<<"Invalid input"<<std::endl;
+    
+    else {
+        displayResults(val);
+    }
 }
 

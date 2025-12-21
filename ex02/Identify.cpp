aa536489853d973ae13e051d56c7468e9ee0a2b0
fran-byte <fran-byte@hotmail.com>
2025-12-21 17:19:55 +0100

    up

--- ex02/Identify.cpp
+++ ex02/Identify.cpp
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 12:49:59 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/21 17:17:23 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/21 17:19:50 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ void identify(Base &p)
         return;
     }
     catch (...)
-    { /* It is not A reference*/
+    { /* This is not a reference to A */
     }
 
     try
@@ -52,7 +52,7 @@ void identify(Base &p)
         return;
     }
     catch (...)
-    { /* It is not B reference*/
+    { /* This is not a reference to B */
     }
 
     try
@@ -62,6 +62,6 @@ void identify(Base &p)
         return;
     }
     catch (...)
-    { /* It is not C reference*/
+    { /* This is not a reference to C */
     }
 }

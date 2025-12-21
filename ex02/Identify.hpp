0adfac611883e53da730d2975386c66351db0e8e
fran-byte <fran-byte@hotmail.com>
2025-12-21 12:50:39 +0100

    up

---
+++ ex02/Identify.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Identify.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 12:46:01 by frromero          #+#    #+#             */
+/*   Updated: 2025/12/21 12:49:01 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IDENTIFY_HPP
+#define IDENTIFY_HPP
+#include "Base.hpp"
+
+Base *generate(void);
+
+void identify(Base *p);
+
+void identify(Base &p);
+
+#endif
